d7053ef4cb83aeb063a65328406a126843047f86
yovie <deasy@oracle_12C.localdomain>
2016-05-18 06:25:39 +0000

    map.js

--- routes/map.js
+++ routes/map.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-  res.render('map', { title: 'Express' });
-});
+app.get('/map', function(req, res) {
+            res.render('views/map', { message: req.flash('loginMessage') });
+        });
+        app.post('/dashboard', passport.authenticate('dashboard', {
+            successRedirect : '/dashboard', // redirect to the secure administrator
+            failureRedirect : '/map', // redirect back to the login page if there is an error
+            failureFlash : true // allow flash messages
+        }));
 
 module.exports = router;
