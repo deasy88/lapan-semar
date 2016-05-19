38cfebdc00252dac4dd90392a80e10c6a2add0c7
yovie <deasy@oracle_12C.localdomain>
2016-05-19 12:31:28 +0000

    dashboard to map

--- routes/map.js
+++ routes/map.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-app.post('/map', function(req, res) {
-            res.render('views/map', { message: req.flash('loginMessage') });
-        });
-        app.post('/dashboard', passport.authenticate('dashboard', {
-            successRedirect : '/dashboard', // redirect to the secure administrator
-            failureRedirect : '/map', // redirect back to the login page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+router.get('/dashboard/map', function(req, res, next) {
+  res.render('views/map', { message: req.flash('livemap') });
+});
 
 module.exports = router;
