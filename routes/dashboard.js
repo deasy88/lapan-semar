4da40b31a97541e3cac36d42a4370865b34d9101
yovie <yoviesmanda@gmail.com>
2016-05-26 10:30:45 +0700

    menu

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,11 +3,31 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  	res.render('dashboard/home', { title: 'Express' });
+  	res.render('dashboard/home', { title: 'Dashboard' });
 });
 
 router.get('/map', function(req, res, next) {
-  	res.render('dashboard/map', { title: 'Express' });
+  	res.render('dashboard/map', { title: 'Live Map' });
+});
+
+router.get('/atmosfer', function(req, res, next) {
+  	res.render('dashboard/map', { title: 'Atmosfer' });
+});
+
+router.get('/laut', function(req, res, next) {
+  	res.render('dashboard/map', { title: 'Laut' });
+});
+
+router.get('/posisi_ikan', function(req, res, next) {
+  	res.render('dashboard/map', { title: 'Posisi Ikan' });
+});
+
+router.get('/satelite', function(req, res, next) {
+  	res.render('dashboard/map', { title: 'Satelite' });
+});
+
+router.get('/land_based', function(req, res, next) {
+  	res.render('dashboard/map', { title: 'Land-Based' });
 });
 
 module.exports = router;
