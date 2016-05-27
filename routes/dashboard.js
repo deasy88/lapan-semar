fe3f7bed15d5cc1c0819a4305b441b6afd629b88
yovie <yoviesmanda@gmail.com>
2016-05-28 06:20:45 +0700

    kapal position

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -12,10 +12,17 @@ router.all('/map', function(req, res, next) {
 	var data = {};
 	data.tanggal = req.body.tanggal;
 	if( data.tanggal!=undefined ) {
-		table.get_where('AIS_POSITION_REPORT', " TANGGAL=TO_DATE('" +data.tanggal+ "','YYYY-MM-DD') ").then( function(result) {
-			data.result = result;
-			console.log( 'result', data );
-			res.render('dashboard/posisi', { title: 'Live Map, Posisi Kapal', view: 1, data:data });
+		table.get_where('AIS_POSITION_REPORT_IND', " TANGGAL=TO_DATE('" +data.tanggal+ "','YYYY-MM-DD') ").then( function(result) {
+			data.result = {};
+			// console.log( result.rows[1] );
+			for(i=0; i<result.rows.length; i++) {
+				if( data.result[ result.rows[i].MMSI ]==undefined ) {
+					data.result[ result.rows[i].MMSI ] = new Array;
+				}
+				data.result[ result.rows[i].MMSI ].push( result.rows[i] );
+			}
+			res.send( data )
+			// res.render('dashboard/posisi', { title: 'Live Map, Posisi Kapal', view: 1, data:data });
 		} );
 	} else {
   		res.render('dashboard/posisi', { title: 'Live Map, Posisi Kapal', view: 1 });
