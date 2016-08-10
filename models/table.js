d7af4b6ce197179e3ac36b51aa441a1617859cc5
yovie <yoviesmanda@gmail.com>
2016-08-11 04:19:23 +0700

    posisi ikan

--- models/table.js
+++ models/table.js
@@ -1,5 +1,17 @@
 var database = require('../services/database.js');
 
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
 module.exports = {
 
 	get_all: function (table) {
@@ -13,7 +25,13 @@ module.exports = {
 	},
 
 	get_last_zppi: function () {
-		var sql = "select z.* from zppi z where z.tanggal=(select * from (select z1.tanggal from zppi z1 order by z1.tanggal desc) where ROWNUM=1)";
+		var d = new Date();
+		d.setDate(d.getDate() - 1);
+		var tgl = formatDate(d);
+		var sql = "select LOCATION_ID,ITEM_NO,TANGGAL,LONGITUDE,LATITUDE,"
+			+ "(TO_DATE('" + tgl + "','YYYY-MM-DD')-TANGGAL) "
+			+ "CLOSEE from ZPPI where (TO_DATE('" + tgl + "','YYYY-MM-DD')-TANGGAL)"
+			+ "<=(select min((TO_DATE('" + tgl + "','YYYY-MM-DD')-TANGGAL)) from zppi) order by CLOSEE asc";
 		return database.simpleExecute( sql, {}, { outFormat: database.OBJECT} );
 	},
 
